f5811da8451b717b0bb61aa39b6fce5c22b3d2b4
flexninja21 <www.edem@gmail.com>
2021-11-19 09:16:39 +0000

    deck

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -5,9 +5,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
-   private List<Card> deck;
+   private List<Card> deck = new ArrayList<>();
+   public CardDeck(){
+      for(CardValue cardValue:CardValue.values()){
+         for (Suit suit:Suit.values()){
+            deck.add(new Card(suit,cardValue));
+
+         }
+      }
+   }
 
 
-
-
+   @Override
+   public String toString() {
+      return "Deck{" +
+              "deck=" + deck +
+              '}';
+   }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	// write your code
 //       Card fred = new Card(Suit.Clubs,CardValue.Ace);
 //        System.out.println(fred);
+        Deck cardde = new Deck();
+        System.out.println(cardde);
 
     }
 }
