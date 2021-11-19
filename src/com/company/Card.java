33fae87a1901278742cf3de0ec936762bf376e75
flexninja21 <www.edem@gmail.com>
2021-11-19 08:29:58 +0000

    created constructor for card class and getters

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -5,7 +5,25 @@ import java.util.List;
 public class Card {
     private Suit suit;
     private CardValue cardValue;
-    private int number;
+
+
+    public Card(Suit suit, CardValue cardValue) {
+        this.suit = suit;
+        this.cardValue = cardValue;
+    }
+// since the Suit is constant we only need setters
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public CardValue getCardValue() {
+        return cardValue;
+    }
+}
+
+
+
+
 
 
 
--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public class Deck {
    private List<Card> card = new ArrayList<>();
-   private  int curCard;
+
 }
