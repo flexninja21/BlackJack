986409c8c508459378b2fd8242166115b25c4cde
flexninja21 <www.edem@gmail.com>
2021-11-19 11:31:33 +0000

    Generating a getHand method Generating a getHand Value method

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,22 +1,28 @@
 package com.company;
 
+import java.util.List;
 import java.util.Stack;
 
 public class Player {
     private String name;
-    private int Score;
+    private int score;
     private Status status;
     private Stack<Card> hand = new Stack<>();
 
     public Player(String name, int score, Status status) {
         this.name = name;
-        Score = score;
+        score = score;
         this.status = status;
     }
 
-    public PlayerHand(){
-
-
-
+    public int getHandValue(){
+        score = 0;
+        for(Card card :getHand() ){
+            score = score + card.getCardValue().getI();
+        }
+        return score;
+    }
+    public Stack<Card> getHand(){
+        return hand;
     }
 }
