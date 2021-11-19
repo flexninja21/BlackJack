3ed15000d0475da6516bbaa18272d0aa72836878
flexninja21 <www.edem@gmail.com>
2021-11-19 08:49:17 +0000

    Starting on the deck

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
-   private List<Card> card = new ArrayList<>();
+   private List<Card> deck;
+
+
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code
-       Card fred = new Card(Suit.Clubs,CardValue.Ace);
-        System.out.println(fred);
+//       Card fred = new Card(Suit.Clubs,CardValue.Ace);
+//        System.out.println(fred);
 
     }
 }
