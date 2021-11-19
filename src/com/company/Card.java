705664567385f333bbfe35d5e687a7c114dfa2bd
flexninja21 <www.edem@gmail.com>
2021-11-19 08:27:28 +0000

    had to refactor entire code ish and changed from the normal mapping to a sort of enum map in the cardvalue

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -4,26 +4,8 @@ import java.util.List;
 
 public class Card {
     private Suit suit;
+    private CardValue cardValue;
     private int number;
 
-    public Card(Suit suit, int number) {
-        this.suit = suit;
-        this.number = number;
-    }
 
-    public Suit getSuit() {
-        return suit;
-    }
 
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public String toString() {
-        return "Card{" +
-                "suit=" + suit +
-                ", number=" + number +
-                '}';
-    }
-}
